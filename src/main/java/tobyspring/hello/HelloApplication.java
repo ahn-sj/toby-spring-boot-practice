763f473e6d501e2b78f5803bed7c89cec95ff8e3
ahn-sj <jaesa5221@gmail.com>
2023-01-21 08:42:40 +0900

    [04-04] 서블릿 요청 처리

--- src/main/java/tobyspring/hello/HelloApplication.java
+++ src/main/java/tobyspring/hello/HelloApplication.java
@@ -3,6 +3,9 @@ package tobyspring.hello;
 import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
 import org.springframework.boot.web.server.WebServer;
 import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
@@ -16,13 +19,17 @@ public class HelloApplication {
 		WebServer webServer = serverFactory.getWebServer(servletContext -> {
 			servletContext.addServlet("hello", new HttpServlet() {
 				@Override
-				protected void service(HttpServletRequest req, HttpServletResponse response) throws ServletException, IOException {
+				protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+					// ====== 요청 ======
+					String name = request.getParameter("name");
+
+					// ====== 응답 ======
 					// 상태 라인, 헤더, HTTP Body
-					response.setStatus(200);
-					response.setHeader("Content-Type", "text/plain");
-					response.getWriter().println("Hello Servlet");
+					response.setStatus(HttpStatus.OK.value()); // OK -> 200
+					response.setHeader(HttpHeaders.CONTENT_TYPE, MediaType.TEXT_PLAIN_VALUE);
+					response.getWriter().println("Hello " + name);
 				}
-			}).addMapping("/hello"); // /hello의 요청이오면 위 service() 메서드가 실행된다
+			}).addMapping("/hello"); // /hello의 요청이오면 위 service() 메서드가 실행
 		});
 		webServer.start(); // 톱캣 웹 서버를 실행
 	}
