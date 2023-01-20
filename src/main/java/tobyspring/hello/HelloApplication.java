15ebce889d7e644536ca2659f6a9541823043375
ahn-sj <jaesa5221@gmail.com>
2023-01-21 08:38:59 +0900

    [04-03] 서블릿 등록

--- src/main/java/tobyspring/hello/HelloApplication.java
+++ src/main/java/tobyspring/hello/HelloApplication.java
@@ -4,10 +4,26 @@ import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactor
 import org.springframework.boot.web.server.WebServer;
 import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
 
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
 public class HelloApplication {
 	public static void main(String[] args) {
 		ServletWebServerFactory serverFactory = new TomcatServletWebServerFactory();
-		WebServer webServer = serverFactory.getWebServer();
+		WebServer webServer = serverFactory.getWebServer(servletContext -> {
+			servletContext.addServlet("hello", new HttpServlet() {
+				@Override
+				protected void service(HttpServletRequest req, HttpServletResponse response) throws ServletException, IOException {
+					// 상태 라인, 헤더, HTTP Body
+					response.setStatus(200);
+					response.setHeader("Content-Type", "text/plain");
+					response.getWriter().println("Hello Servlet");
+				}
+			}).addMapping("/hello"); // /hello의 요청이오면 위 service() 메서드가 실행된다
+		});
 		webServer.start(); // 톱캣 웹 서버를 실행
 	}
 }
