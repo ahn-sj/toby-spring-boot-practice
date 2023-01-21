2a5753641ee22d20859c279ab50fd3befee486fa
ahn-sj <jaesa5221@gmail.com>
2023-01-21 10:51:49 +0900

    [05-01] 스프링 컨테이너 사용

--- src/main/java/tobyspring/hello/HelloApplication.java
+++ src/main/java/tobyspring/hello/HelloApplication.java
@@ -3,7 +3,7 @@ package tobyspring.hello;
 import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
 import org.springframework.boot.web.server.WebServer;
 import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
-import org.springframework.http.HttpHeaders;
+import org.springframework.context.support.GenericApplicationContext;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
@@ -16,28 +16,34 @@ import java.io.IOException;
 
 public class HelloApplication {
 	public static void main(String[] args) {
+		GenericApplicationContext applicationContext = new GenericApplicationContext(); // Spring Container
+		applicationContext.registerBean(HelloController.class); // 빈 등록
+		applicationContext.refresh();
+
 		ServletWebServerFactory serverFactory = new TomcatServletWebServerFactory();
 		WebServer webServer = serverFactory.getWebServer(servletContext -> {
-			HelloController helloController = new HelloController();
-
-			servletContext.addServlet("frontcontroller", new HttpServlet() {
+			servletContext.addServlet("hello", new HttpServlet() {
 				@Override
 				protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+					// 인증, 보안, 다국어, 공통 기능
+
 					// ====== 요청 ======
 					// GET으로 오는 /hello의 URL만 가능
 					if (request.getRequestURI().equals("/hello") && request.getMethod().equals(HttpMethod.GET.name())) {
 						String name = request.getParameter("name");
-						String rst = helloController.hello(name);
 
+						// 스프링 컨테이너가 가지는 helloController 가져오기
+						HelloController helloController = applicationContext.getBean(HelloController.class);
+						String rst = helloController.hello(name);
 
 						// ====== 응답 ======
 						// 상태 라인, 헤더, HTTP Body
-						response.setStatus(HttpStatus.OK.value()); // OK -> 200
-						response.setHeader(HttpHeaders.CONTENT_TYPE, MediaType.TEXT_PLAIN_VALUE);
+//						response.setStatus(HttpStatus.OK.value()); // DEFAULT
+//						response.setHeader(HttpHeaders.CONTENT_TYPE, MediaType.TEXT_PLAIN_VALUE);
+						response.setContentType(MediaType.TEXT_PLAIN_VALUE);
 						response.getWriter().println(rst);
-					} else if (request.getRequestURI().equals("/user")) {
-						//
-					} else {
+					}
+					 else {
 						response.setStatus(HttpStatus.NOT_FOUND.value());
 					}
 
