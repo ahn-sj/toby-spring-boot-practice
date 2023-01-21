69100b521da35c80cd27067eafa074a370d23ec2
ahn-sj <jaesa5221@gmail.com>
2023-01-21 13:50:49 +0900

    [05-07] 스프링 컨테이너로 통합

--- src/main/java/tobyspring/hello/HelloApplication.java
+++ src/main/java/tobyspring/hello/HelloApplication.java
@@ -8,17 +8,22 @@ import org.springframework.web.servlet.DispatcherServlet;
 
 public class HelloApplication {
 	public static void main(String[] args) {
-		GenericWebApplicationContext applicationContext = new GenericWebApplicationContext(); // Spring Container
+		GenericWebApplicationContext applicationContext = new GenericWebApplicationContext() {
+			@Override
+			protected void onRefresh() {
+				super.onRefresh();
+
+				ServletWebServerFactory serverFactory = new TomcatServletWebServerFactory();
+				WebServer webServer = serverFactory.getWebServer(servletContext -> {
+					servletContext.addServlet("dispatcherServlet",
+							new DispatcherServlet(this)
+					).addMapping("/*"); // 모든 요청을 front controller 가 받도록 변경
+				});
+				webServer.start(); // 톱캣 웹 서버를 실행
+			}
+		}; // Spring Container
 		applicationContext.registerBean(HelloController.class); // 빈 등록
 		applicationContext.registerBean(SimpleHelloService.class); // 빈 등록
 		applicationContext.refresh();
-
-		ServletWebServerFactory serverFactory = new TomcatServletWebServerFactory();
-		WebServer webServer = serverFactory.getWebServer(servletContext -> {
-			servletContext.addServlet("dispatcherServlet",
-					new DispatcherServlet(applicationContext)
-			).addMapping("/*"); // 모든 요청을 front controller 가 받도록 변경
-		});
-		webServer.start(); // 톱캣 웹 서버를 실행
 	}
 }
