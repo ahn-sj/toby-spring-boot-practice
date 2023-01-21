7b1d6854547d80097ac9b919f333acf47dc09a93
ahn-sj <jaesa5221@gmail.com>
2023-01-21 14:34:39 +0900

    [05-10] Bean의 생명주기 메서드

--- src/main/java/tobyspring/hello/HelloApplication.java
+++ src/main/java/tobyspring/hello/HelloApplication.java
@@ -3,6 +3,7 @@ package tobyspring.hello;
 import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
 import org.springframework.boot.web.server.WebServer;
 import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
@@ -12,15 +13,15 @@ import org.springframework.web.servlet.DispatcherServlet;
 @ComponentScan
 public class HelloApplication {
 
-//	@Bean
-//	public HelloController helloController(HelloService helloService) {
-//		return new HelloController(helloService);
-//	}
-//
-//	@Bean
-//	public HelloService helloService() {
-//		return new SimpleHelloService();
-//	}
+	@Bean
+	public ServletWebServerFactory servletWebServerFactory() {
+		return new TomcatServletWebServerFactory();
+	}
+
+	@Bean DispatcherServlet dispatcherServlet() {
+		return new DispatcherServlet();
+	}
+
 
 	public static void main(String[] args) {
 		AnnotationConfigWebApplicationContext applicationContext = new AnnotationConfigWebApplicationContext() {
@@ -28,11 +29,13 @@ public class HelloApplication {
 			protected void onRefresh() {
 				super.onRefresh();
 
-				ServletWebServerFactory serverFactory = new TomcatServletWebServerFactory();
+				ServletWebServerFactory serverFactory = this.getBean(ServletWebServerFactory.class);
+				DispatcherServlet dispatcherServlet = this.getBean(DispatcherServlet.class);
+//				dispatcherServlet.setApplicationContext(this); // 생략 가능
+
 				WebServer webServer = serverFactory.getWebServer(servletContext -> {
-					servletContext.addServlet("dispatcherServlet",
-							new DispatcherServlet(this)
-					).addMapping("/*"); // 모든 요청을 front controller 가 받도록 변경
+					servletContext.addServlet("dispatcherServlet", dispatcherServlet)
+							.addMapping("/*"); // 모든 요청을 front controller 가 받도록 변경
 				});
 				webServer.start(); // 톱캣 웹 서버를 실행
 			}
