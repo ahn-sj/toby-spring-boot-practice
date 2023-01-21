b54f65e793ca25b7170d890f29c0fbc8c5364a94
ahn-sj <jaesa5221@gmail.com>
2023-01-21 14:41:10 +0900

    [05-11] SpringBootApplication

--- src/main/java/tobyspring/hello/HelloApplication.java
+++ src/main/java/tobyspring/hello/HelloApplication.java
@@ -1,12 +1,11 @@
 package tobyspring.hello;
 
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
-import org.springframework.boot.web.server.WebServer;
 import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
 import org.springframework.web.servlet.DispatcherServlet;
 
 @Configuration
@@ -22,25 +21,8 @@ public class HelloApplication {
 		return new DispatcherServlet();
 	}
 
-
 	public static void main(String[] args) {
-		AnnotationConfigWebApplicationContext applicationContext = new AnnotationConfigWebApplicationContext() {
-			@Override
-			protected void onRefresh() {
-				super.onRefresh();
-
-				ServletWebServerFactory serverFactory = this.getBean(ServletWebServerFactory.class);
-				DispatcherServlet dispatcherServlet = this.getBean(DispatcherServlet.class);
-//				dispatcherServlet.setApplicationContext(this); // 생략 가능
-
-				WebServer webServer = serverFactory.getWebServer(servletContext -> {
-					servletContext.addServlet("dispatcherServlet", dispatcherServlet)
-							.addMapping("/*"); // 모든 요청을 front controller 가 받도록 변경
-				});
-				webServer.start(); // 톱캣 웹 서버를 실행
-			}
-		}; // Spring Container
-		applicationContext.register(HelloApplication.class);
-		applicationContext.refresh();
+//		MySpringApplication.run(HelloApplication.class, args); // Custom SpringApplication
+		SpringApplication.run(HelloApplication.class, args);
 	}
 }
