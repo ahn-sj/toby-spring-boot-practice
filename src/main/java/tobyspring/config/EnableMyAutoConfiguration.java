7c1912a77bab6dae0f70045b57495a55183d48d5
ahn-sj <jaesa5221@gmail.com>
2023-01-22 23:21:00 +0900

    [07-03] 인프라 빈 구성 정보의 분리

---
+++ src/main/java/tobyspring/config/EnableMyAutoConfiguration.java
@@ -0,0 +1,16 @@
+package tobyspring.config;
+
+import org.springframework.context.annotation.Import;
+import tobyspring.config.autoconfig.DispatcherServletConfig;
+import tobyspring.config.autoconfig.TomcatWebServerConfig;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Target(ElementType.TYPE)
+@Retention(RetentionPolicy.RUNTIME)
+@Import({DispatcherServletConfig.class, TomcatWebServerConfig.class})
+public @interface EnableMyAutoConfiguration {
+}
--- src/main/java/tobyspring/hello/HelloApplication.java
+++ src/main/java/tobyspring/hello/HelloApplication.java
@@ -1,29 +1,11 @@
 package tobyspring.hello;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
-import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.DispatcherServlet;
+import tobyspring.config.MySpringBootApplication;
 
-//@Configuration
-//@ComponentScan
-@MySpringBootAnnotation
+@MySpringBootApplication
 public class HelloApplication {
-
-//	@Bean
-//	public ServletWebServerFactory servletWebServerFactory() {
-//		return new TomcatServletWebServerFactory();
-//	}
-//
-//	@Bean DispatcherServlet dispatcherServlet() {
-//		return new DispatcherServlet();
-//	}
-
 	public static void main(String[] args) {
-//		MySpringApplication.run(HelloApplication.class, args); // Custom SpringApplication
 		SpringApplication.run(HelloApplication.class, args);
 	}
 }
