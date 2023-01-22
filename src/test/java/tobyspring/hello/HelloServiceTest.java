6ea2009783c453cdb038d18d4bde2a26e31073ae
ahn-sj <jaesa5221@gmail.com>
2023-01-22 22:56:05 +0900

    [07-01] 메타 애노테이션과 합성 애노테이션

--- src/test/java/tobyspring/hello/HelloServiceTest.java
+++ src/test/java/tobyspring/hello/HelloServiceTest.java
@@ -3,8 +3,25 @@ package tobyspring.hello;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+//@Retention(RetentionPolicy.RUNTIME)
+//@Target(ElementType.METHOD)
+//@UnitTest
+//@interface FastUnitTest {}
+//
+//@Retention(RetentionPolicy.RUNTIME)
+//@Target({ElementType.ANNOTATION_TYPE, ElementType.METHOD})
+//@Test
+//@interface UnitTest {
+//}
+
 public class HelloServiceTest {
 
+//    @FastUnitTest
     @Test
     void simpleHelloService() throws Exception {
         SimpleHelloService helloService = new SimpleHelloService();
@@ -13,6 +30,7 @@ public class HelloServiceTest {
         Assertions.assertThat(rst).isEqualTo("Hello Test");
     }
 
+//    @UnitTest
     @Test
     void helloDecorator() throws Exception {
         HelloDecorator decorator = new HelloDecorator(name -> name);
